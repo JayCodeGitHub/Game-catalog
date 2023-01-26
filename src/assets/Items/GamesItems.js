0ba4d6cd63fd9559de1896e42ad6406d62f6ba43
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-01-26 20:49:19 +0100

    Added Items

--- src/assets/Items/GamesItems.js
+++ src/assets/Items/GamesItems.js
@@ -1,8 +1,22 @@
 import HogwartsLegacy from "../images/HogwartsLegacy.jpeg"
+import TheLastofUsPart1 from "../images/TheLastofUsPart1.jpeg"
+import AssassinsCreedValhalla from "../images/AssassinsCreedValhalla.jpeg"
+import CallofDutyModernWarfareII from "../images/CallofDutyModernWarfareII.jpeg"
+import LikeaDragonIshin from "../images/LikeaDragonIshin.jpeg"
+import MarvelsSpiderManMilesMorales from "../images/MarvelsSpiderManMilesMorales.jpeg"
+import StarWarsJediSurvivor from "../images/StarWarsJediSurvivor.jpeg"
+import GrandTheftAutoV from "../images/GrandTheftAutoV.jpeg"
+import GrandTheftAutoVXSX from "../images/GrandTheftAutoVXSX.jpeg"
 
 export const GamesItems = [
-    { title: 'Hogwarts Legacy', platform:'PS5', genre:'Action', thumbnail:HogwartsLegacy},
-    { title: 'The Last of Us Part 1', platform:'PS5', genre:'Action', thumbnail:HogwartsLegacy},
-    { title: 'Hogwarts Legacy', platform:'PS5', genre:'Action', thumbnail:HogwartsLegacy},
+    { title: `Hogwarts Legacy`, platform:'PS5', genre:'action', thumbnail:HogwartsLegacy},
+    { title: `The Last of Us Part 1`, platform:'PS5', genre:'action', thumbnail:TheLastofUsPart1},
+    { title: `Assassin's Creed Valhalla`, platform:'XSX', genre:'adventure', thumbnail:AssassinsCreedValhalla},
+    { title: `Call of Duty: Modern Warfare II`, platform:'PS5', genre:'multiplayer', thumbnail:CallofDutyModernWarfareII},
+    { title: `Like a Dragon: Ishin`, platform:'XSX', genre:'action', thumbnail:LikeaDragonIshin},
+    { title: `Marvel's Spider-Man: Miles Morales`, platform:'PS5', genre:'action', thumbnail:MarvelsSpiderManMilesMorales},
+    { title: `Star Wars Jedi: Survivor`, platform:'PS5', genre:'action', thumbnail:StarWarsJediSurvivor},
+    { title: `Grand Theft Auto V`, platform:'PS5', genre:'action', thumbnail:GrandTheftAutoV},
+    { title: `Grand Theft Auto V`, platform:'XSX', genre:'action', thumbnail:GrandTheftAutoVXSX},
   ]
   
