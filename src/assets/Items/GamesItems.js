41c06846cd5a33a69672235a29382427f509cad4
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-01-26 22:15:45 +0100

    Added more items

--- src/assets/Items/GamesItems.js
+++ src/assets/Items/GamesItems.js
@@ -1,22 +1,32 @@
-import HogwartsLegacy from "../images/HogwartsLegacy.jpeg"
 import TheLastofUsPart1 from "../images/TheLastofUsPart1.jpeg"
-import AssassinsCreedValhalla from "../images/AssassinsCreedValhalla.jpeg"
-import CallofDutyModernWarfareII from "../images/CallofDutyModernWarfareII.jpeg"
-import LikeaDragonIshin from "../images/LikeaDragonIshin.jpeg"
-import GrandTheftAutoVXSX from "../images/GrandTheftAutoVXSX.jpeg"
 import MarvelsSpiderManMilesMorales from "../images/MarvelsSpiderManMilesMorales.jpeg"
-import StarWarsJediSurvivor from "../images/StarWarsJediSurvivor.jpeg"
 import GrandTheftAutoV from "../images/GrandTheftAutoV.jpeg"
+import Battlefield2042 from "../images/Battlefield2042.jpeg"
+import HogwartsLegacy from "../images/HogwartsLegacy.jpeg"
+import LikeaDragonIshin from "../images/LikeaDragonIshin.jpeg"
+import HITMAN3 from "../images/HITMAN3.jpeg"
+import AssassinsCreedValhalla from "../images/AssassinsCreedValhalla.jpeg"
+import ResidentEvil4 from "../images/ResidentEvil4.jpeg"
+import StarWarsJediSurvivor from "../images/StarWarsJediSurvivor.jpeg"
+import NBA2K23 from "../images/NBA2K23.jpeg"
+import CallofDutyModernWarfareII from "../images/CallofDutyModernWarfareII.jpeg"
+import GrandTheftAutoVXSX from "../images/GrandTheftAutoVXSX.jpeg"
+import WatchDogsLegion from "../images/WatchDogsLegion.jpeg"
 
 export const GamesItems = [
-    { title: `Hogwarts Legacy`, platform:'PS5', genre:'Action', thumbnail:HogwartsLegacy},
-    { title: `The Last of Us Part 1`, platform:'PS5', genre:'Action', thumbnail:TheLastofUsPart1},
-    { title: `Assassin's Creed Valhalla`, platform:'XSX', genre:'Adventure', thumbnail:AssassinsCreedValhalla},
-    { title: `Call of Duty: Modern Warfare II`, platform:'PS5', genre:'Multiplayer', thumbnail:CallofDutyModernWarfareII},
+  { title: `Marvel's Spider-Man: Miles Morales`, platform:'PS5', genre:'Action', thumbnail:MarvelsSpiderManMilesMorales},
+  { title: `The Last of Us Part 1`, platform:'PS5', genre:'Action', thumbnail:TheLastofUsPart1},
+  { title: `Grand Theft Auto V`, platform:'PS5', genre:'Action', thumbnail:GrandTheftAutoV},
+  { title: `Battlefield 2042`, platform:'XSX', genre:'Multiplayer', thumbnail:Battlefield2042},
+  { title: `Hogwarts Legacy`, platform:'PS5', genre:'Action', thumbnail:HogwartsLegacy},
     { title: `Like a Dragon: Ishin`, platform:'XSX', genre:'Action', thumbnail:LikeaDragonIshin},
-    { title: `Grand Theft Auto V`, platform:'XSX', genre:'Action', thumbnail:GrandTheftAutoVXSX},
-    { title: `Marvel's Spider-Man: Miles Morales`, platform:'PS5', genre:'Action', thumbnail:MarvelsSpiderManMilesMorales},
+    { title: `HITMAN 3`, platform:'XSX', genre:'Action', thumbnail:HITMAN3},
+    { title: `Assassin's Creed Valhalla`, platform:'XSX', genre:'Adventure', thumbnail:AssassinsCreedValhalla},
+    { title: `Resident Evil 4`, platform:'XSX', genre:'Action', thumbnail:ResidentEvil4},
     { title: `Star Wars Jedi: Survivor`, platform:'PS5', genre:'Action', thumbnail:StarWarsJediSurvivor},
-    { title: `Grand Theft Auto V`, platform:'PS5', genre:'Action', thumbnail:GrandTheftAutoV},
+    { title: `NBA 2K23`, platform:'XSX', genre:'Sports', thumbnail:NBA2K23},
+    { title: `Call of Duty: Modern Warfare II`, platform:'PS5', genre:'Multiplayer', thumbnail:CallofDutyModernWarfareII},
+    { title: `Grand Theft Auto V`, platform:'XSX', genre:'Action', thumbnail:GrandTheftAutoVXSX},
+    { title: `Watch Dogs: Legion`, platform:'XSX', genre:'Action', thumbnail:WatchDogsLegion},
   ]
   
