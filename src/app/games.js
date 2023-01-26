696a8e78eeb3c7baf9ba485eb718299457d1fe2c
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-01-26 19:01:28 +0100

    Render games with js script

--- src/app/games.js
+++ src/app/games.js
@@ -1,7 +1,33 @@
 import { GamesItems } from '../assets/Items/GamesItems'
 
 function games() {
-    console.log(GamesItems);
+    const games = document.getElementById("games");
+
+    GamesItems.forEach((item) => {
+        const game = document.createElement('div');
+        game.id = 'game';
+
+        const title = document.createElement('h2');
+        title.innerHTML = item.title
+
+        const img = document.createElement('img');
+        img.src = item.thumbnail
+
+        const platform = document.createElement('h3');
+        platform.innerHTML = 'platform: '
+        const platformItem = document.createElement('span');
+        platformItem.innerHTML = item.platform
+        platform.appendChild(platformItem);
+
+        const genre = document.createElement('h3');
+        genre.innerHTML = 'genre: '
+        const genreItem = document.createElement('span');
+        genreItem.innerHTML = item.genre
+        genre.appendChild(genreItem)
+
+        game.append(img, title, platform, genre)
+        games.appendChild(game);
+    })
 }
 
 export default games
--- src/assets/Items/GamesItems.js
+++ src/assets/Items/GamesItems.js
@@ -2,5 +2,7 @@ import HogwartsLegacy from "../images/HogwartsLegacy.jpeg"
 
 export const GamesItems = [
     { title: 'Hogwarts Legacy', platform:'PS5', genre:'Action', thumbnail:HogwartsLegacy},
+    { title: 'Hogwarts Legacy', platform:'PS5', genre:'Action', thumbnail:HogwartsLegacy},
+    { title: 'Hogwarts Legacy', platform:'PS5', genre:'Action', thumbnail:HogwartsLegacy},
   ]
   
